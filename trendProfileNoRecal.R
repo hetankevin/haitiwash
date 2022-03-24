4f41406d4d65f4e2ea533a567f809a5fbc9d0ab7
hetankevin <kevtan@umich.edu>
2022-03-24 01:26:56 -0400

    update

--- trendProfileNoRecal.R
+++ trendProfileNoRecal.R
@@ -380,7 +380,7 @@ rw_sd1 <- rw.sd(beta1 = .02, beta2 = .02, beta3 = .02,
 
 freeze(seed=1196696958,
        profile_design(
-         betat=seq(-0.07,0.04,length=50),
+         betat=seq(-0.07,0.1,length=100),
          lower=box[1,unfixed_param_names],
          upper=box[2,unfixed_param_names],
          nprof=10, type="runif"
