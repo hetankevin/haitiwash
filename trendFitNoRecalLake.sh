18170f4d82b248d64ecc8078a0a52482f3eab775
hetankevin <kevtan@umich.edu>
2022-03-29 15:53:46 -0400

    fit

---
+++ trendFitNoRecalLake.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=trendProfile
+#SBATCH --mail-user=kevtan@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+ 
+#SBATCH --account=stats_dept1
+#SBATCH --partition=standard
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --cpus-per-task=1
+
+## 5GB/cpu is the basic share
+#SBATCH --mem-per-cpu=1GB
+
+## wall time hours:minutes:seconds
+#SBATCH --time=3-00:00:00
+
+###   Load software modules
+
+module load R/4.1.0
+module list
+
+####  Commands your job should run follow this line
+
+echo "Running on $SLURM_JOB_NODELIST"
+echo "Running in $(pwd)"
+
+Rscript trendFitNoRecal.R
